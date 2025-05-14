f51c3cb95abc4e757c6c14fc26cdf48cd058a260
Arthur Melo <arthur7m@gmail.com>
2025-05-14 10:21:52 -0300

    fix(cors): Soluciona erro de cors da api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import { connectDB } from './db/db';
 import authRouter from './routes/authRouter';
 import logger from './utils/logger/logger';
 import todoRouter from './routes/todoRouter';
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
@@ -12,6 +13,20 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'Cache-Control',
+    'Pragma'
+  ],
+  exposedHeaders: ['Authorization'],
+  credentials: true,
+  maxAge: 86400
+}));
 app.use("/api/auth", authRouter);
 app.use("/api/v1/todos", todoRouter);
 

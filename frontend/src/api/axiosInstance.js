c9f1f1caa033ff805f4062afde199e601b5eb119
Arthur Melo <arthur7m@gmail.com>
2025-07-10 15:56:02 -0300

    chore(docker): Adiciona Dockerfile e docker-compose

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.SERVER_URL || 'http://localhost:4000/api',
+  baseURL: import.meta.env.VITE_SERVER_URL  || 'http://localhost:4000/api',
 });
 
 API.interceptors.request.use((config) => {
